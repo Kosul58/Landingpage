020b24925e3877c27485b47db282af8d15d2969b
Kosul58 <kosulgrg@gmail.com>
2024-07-27 09:17:38 +0545

    a

--- Landingpage/fooddata/diab_lbp.js
+++ Landingpage/fooddata/diab_lbp.js
@@ -1,4 +1,4 @@
-[
+let originalArray = [
   [805, 45, 75, 1000, 10, 0, 1, 0],
   [805, 45, 75, 1000, 8, 0, 1, 0],
   [805, 45, 75, 1000, 11, 0, 1, 0],
@@ -108,3 +108,29 @@
   [905, 55, 75, 700, 8, 0, 1, 0],
   [905, 55, 75, 700, 11, 0, 1, 0],
 ];
+
+// Function to update the first element
+function updateFirstElement(array) {
+  // Iterate through each sub-array
+  return array.map((subArray) => {
+    // If the first element is 1000, change it to 300
+    if (subArray[5] === 0) {
+      subArray[5] = 1;
+    }
+    return subArray;
+  });
+}
+
+// Create a new array with the updated elements
+let newArray = updateFirstElement(originalArray);
+
+const fs = require("fs");
+
+const jsonData = JSON.stringify(newArray, null, 2);
+fs.writeFile("asper.js", jsonData, (err) => {
+  if (err) {
+    console.error("Error writing file", err);
+  } else {
+    console.log("File has been saved");
+  }
+});
